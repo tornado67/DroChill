a27d73bba8a4f47c78c3eb8f66a54b5279ca83ca
Aleksei Ruzhitskii <alexey.ruzhitskiy@high5games.com>
2022-02-05 01:17:53 +0100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+#include <BleMouse.h>
+#define MINUTE 60000
+
+BleMouse bleMouse("DroChill mouse jiggler v1");
+ 
+int move_interval = 3;
+int loop_interval = MINUTE * 1;
+
+void setup()
+{
+   bleMouse.begin();
+}
+
+void loop()
+{
+
+  int distance = random(10, 800);
+  int x = random(3) - 1;
+  int y = random(3) - 1;
+  for (int i = 0; i < distance; i++) {
+    
+    bleMouse.move(x, y, 0);
+    delay(move_interval);
+    if (random(1,10) == 2 || random (1,10) == 5){
+        //bleMouse.click(MOUSE_RIGHT);
+    }
+  }
+  delay(loop_interval);
+
+}
